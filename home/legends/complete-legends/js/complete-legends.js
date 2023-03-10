73020d22a30a9b0c4f7d2b9433b3ff35a3ed22da
Bianca Gomes <bibi.gomes.f@gmail.com>
2023-03-10 11:47:42 -0300

    Ajusta as páginas das lendas

--- home/legends/complete-legends/js/complete-legends.js
+++ home/legends/complete-legends/js/complete-legends.js
@@ -1,3 +1,11 @@
+function goToHome() {
+    window.location.href = "../../../../../home/home.html"
+}
+
 function goToLegends() {
     window.location.href = "../../../../legends/legends.html"
+}
+
+function goToCasesWithoutSolution() {
+    window.location.href = "../../../../cases-without-solution/cases-without-solution.html"
 }
