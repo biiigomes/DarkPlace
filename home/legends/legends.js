5ed08881e6443d87097605ed5318f702c3205c52
Bianca Gomes <bibi.gomes.f@gmail.com>
2023-03-10 10:08:55 -0300

    Ajusta lendas

--- home/legends/legends.js
+++ home/legends/legends.js
@@ -1,3 +1,15 @@
 function goToCasesWithoutSolution() {
-    window.location.href = "../cases-without-solution/cases-without-solution.html"
+    window.location.href = "/../cases-without-solution/cases-without-solution.html"
+}
+
+function goToLegend() {
+    if ("#iron-witch") {
+        window.location.href = "complete-legends/iron-witch/iron-witch.html"
+    } else if ("#yule-cat") {
+        window.location.href = "complete-legends/yule-cat/yule-cat.html"
+    } else if ("#lent-dog") {
+        window.location.href = "complete-legends/lent-dog/lent-dog.html"
+    } else {
+        window.location.href = "complete-legends/labatut/labatut.html"
+    }
 }
