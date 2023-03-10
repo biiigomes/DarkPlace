ae451f5c2bdb4fcbb276b8db09607fdca4217292
Bianca Gomes <bibi.gomes.f@gmail.com>
2023-03-10 11:31:25 -0300

    Atualiza html das páginas das lendas e as organiza

--- home/legends/legends.js
+++ home/legends/legends.js
@@ -9,15 +9,15 @@ function goToCasesWithoutSolution() {
 function goToCompleteLegend(legend) {
     switch (legend) {
         case "iron-witch":
-            window.location.href = "complete-legends/iron-witch/iron-witch.html";
+            window.location.href = "complete-legends/pages/iron-witch/iron-witch.html";
         break;
         case "yule-cat":
-            window.location.href = "complete-legends/yule-cat/yule-cat.html";
+            window.location.href = "complete-legends/pages/yule-cat/yule-cat.html";
         break;
         case "lent-dog":
-            window.location.href = "complete-legends/lent-dog/lent-dog.html";
+            window.location.href = "complete-legends/pages/lent-dog/lent-dog.html";
         break;
         default:
-            window.location.href = "complete-legends/labatut/labatut.html"
+            window.location.href = "complete-legends/pages/labatut/labatut.html"
     }
 }
